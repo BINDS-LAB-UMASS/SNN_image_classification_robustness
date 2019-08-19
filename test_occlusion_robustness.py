c2ec4c6115b1f3424408bbbf3fcb159c42338a61
devdharpatel <devdharpatel@gmail.com>
2019-08-19 17:32:15 -0400

    minor changes

--- test_occlusion_robustness.py
+++ test_occlusion_robustness.py
@@ -9,6 +9,20 @@ import pandas as pd
 import argparse
 
 
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 1000)
+        self.fc2 = nn.Linear(1000, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
 def main(occlusion_percentage=0):
 
     percentile = 99.9
@@ -37,18 +51,7 @@ def main(occlusion_percentage=0):
             mask = torch.Tensor(img.shape[1], img.shape[2]).uniform_() > self.percentage
             return img * mask.float()
 
-    class Net(nn.Module):
 
-        def __init__(self):
-            super(Net, self).__init__()
-            self.fc1 = nn.Linear(28 * 28, 1000)
-            self.fc2 = nn.Linear(1000, 10)
-
-        def forward(self, x):
-            x = x.view(-1, 28 * 28)
-            x = F.relu(self.fc1(x))
-            x = self.fc2(x)
-            return F.log_softmax(x)
 
     train_dataset2 = datasets.MNIST('./data',
                                        train=False,
